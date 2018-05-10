64115ab34fad9508e092675868bf0f3df15d3611
jing1127 <1078127666@qq.com>
2018-05-10 17:16:15 +0800

    违章处理

---
+++ js/tabs.js
@@ -0,0 +1,11 @@
+
+function tabs(){
+	location.href="ordernone.html?merId="+merId;
+}
+function tab1(){
+	location.href="orderown.html?merId="+merId;
+}
+function tab2(){
+	location.href="orderok.html?merId="+merId;
+}
+
